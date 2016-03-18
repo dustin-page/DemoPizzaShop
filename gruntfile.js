6c2d9ae455f42792cbfe396f7541f0f203a8eeda
dustin-page <dustinpage@live.com>
2016-03-18 16:11:59 -0500

    Initial drop of project

---
+++ gruntfile.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 3/18/16.
+ */
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        jshint: {
+            all: ["./public/js/**/*.js"],
+            options: {
+                unused: true
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+
+    grunt.registerInitTask("default", ["jshint"]);
+};
